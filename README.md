1920d51ceb4b2be9f3f326c9a69ec30a4a9bb794
nresh <mail2naresh92@gmail.com>
2021-08-02 08:15:40 -0400

    adding tic-tac-toe-library to vue app

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vue-app
-Vue app using an Vue component library built by me
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <TicTacToe :player="1" />
+  </div>
+</template>
+
+<script>
+import TicTacToe from "tic-tac-toe-library/src/components/TicTacToe.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    TicTacToe,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
